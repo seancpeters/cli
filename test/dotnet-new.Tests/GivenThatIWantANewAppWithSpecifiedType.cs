4400485a23800997a95709fc57e35e24ed06a129
seancpeters <sepeters@microsoft.com>
2017-01-20 20:41:27 -0800

    new cache initialization

---
+++ test/dotnet-new.Tests/GivenThatIWantANewAppWithSpecifiedType.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.DotNet.TestFramework;
+using Microsoft.DotNet.Tools.Test.Utilities;
+using Xunit;
+using FluentAssertions;
+using Microsoft.DotNet.Cli.Utils;
+
+namespace Microsoft.DotNet.New.Tests
+{
+    public class GivenThatIWantANewAppWithSpecifiedType : NewTestBase
+    {
+        [Theory]
+        [InlineData("C#", "console", false)]
+        [InlineData("C#", "classlib", false)]
+        [InlineData("C#", "mstest", false)]
+        [InlineData("C#", "xunit", false)]
+        [InlineData("C#", "web", true)]
+        [InlineData("C#", "mvc", true)]
+        [InlineData("C#", "webapi", true)]
+        [InlineData("F#", "console", false)]
+        [InlineData("F#", "classlib", false)]
+        [InlineData("F#", "mstest", false)]
+        [InlineData("F#", "xunit", false)]
+        [InlineData("F#", "mvc", true)]
+        public void TemplateRestoresAndBuildsWithoutWarnings(
+            string language,
+            string projectType,
+            bool useNuGetConfigForAspNet)
+        {
+            string rootPath = TestAssetsManager.CreateTestDirectory(identifier: $"{language}_{projectType}").Path;
+
+            new TestCommand("dotnet") { WorkingDirectory = rootPath }
+                .Execute($"new {projectType} -lang {language} -o {rootPath}")
+                .Should().Pass();
+
+            if (useNuGetConfigForAspNet)
+            {
+                var configFile = new FileInfo(Path.Combine(rootPath,"..","..","..","..","NuGet.tempaspnetpatch.config"));
+                File.Copy(configFile.FullName, Path.Combine(rootPath, "NuGet.Config"));
+            }
+
+            // note (scp 2017-01-17): we keep going back & forth whether to have global.json in the templates.
+            //  at this point we do not, but when we do, this part of the test will become valid again.
+            //string globalJsonPath = Path.Combine(rootPath, "global.json");
+            //Assert.True(File.Exists(globalJsonPath));
+            //Assert.Contains(Product.Version, File.ReadAllText(globalJsonPath));
+
+            new TestCommand("dotnet")
+                .WithWorkingDirectory(rootPath)
+                .Execute($"restore")
+                .Should().Pass();
+
+            var buildResult = new TestCommand("dotnet")
+                .WithWorkingDirectory(rootPath)
+                .ExecuteWithCapturedOutput("build")
+                .Should().Pass()
+                .And.NotHaveStdErr();
+        }
+    }
+}
